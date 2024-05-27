74b27a2502ed64ed8292abff983a6bc81ab441a2
JoseL2119 <128648766+JoseL2119@users.noreply.github.com>
2024-05-27 00:23:37 -0400

    Versión BETA
    optamos por hacer que el juego fuese infinito y que conste de 3 rondas principales:
    
    RONDA 1 (0-2min):
    Comprende desde el segundo 0 hasta el segundo 120 de partida. Los tiempos de aparición de los personajes varían entre los 2 y los 4 segundos. Los porcentajes de aparición de cada enemigo son los siguientes:
    - Araña gris: 50%
    - Araña dorada: 30%
    - Dron: 19.8%
    - Tanque: 0.2%
    
    RONDA 2 (2-5min):
    Comprende desde el segundo 120 hasta el segundo 300 de partida. Los tiempos de aparición de los personajes varían entre los 1 y los 3 segundos. Los porcentajes de aparición de cada enemigo son los siguientes:
    - Araña gris: 25%
    - Araña dorada: 45%
    - Dron: 27%
    - Tanque: 3%
    
    RONDA 3 (>5min):
    Comprende desde el segundo 300 en adelante. Los tiempos de aparición de los enemigos varían entre los 0.5 y los 2.5 segundos. Los porcentajes de aparición de cada enemigo son los siguientes:
    - Araña gris: 10%
    - Araña dorada: 40%
    - Dron: 40%
    - Tanque: 10%
    
    la idea es que nadie pueda aguantar más de 5 minutos y poco dentro del juego. como verán a continuación en los vídeos, ya a partir de la segunda fase el juego se hace bastante difícil. se tiene en mente implementar un sistema de drops para que el jugador pueda aguantar más tiempo dentro de la partida (incluyen vida y mejoras de daño e invulnerabilidad).

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EnemySpawner : MonoBehaviour
 {   
@@ -11,6 +13,8 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject enemyPrefab4;
     [SerializeField] private float minimumSpawnTime;
     [SerializeField] private float maximumSpawnTime;
+    [SerializeField] private TextMeshProUGUI textoTimer; 
+    public float timer = 0;
     private float timeUntilSpawn;
 
 
@@ -22,39 +26,121 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float peso1 = 50;
-        float peso2 = 30;
-        float peso3 = 19.8f;
-        float peso4 = 0.2f;
-        float pesoTotal = peso1 + peso2 + peso3 + peso4;
-        // float pesoAcumulado = 0;
-        float ranEnemy = Random.Range(0, pesoTotal);  
-        timeUntilSpawn -= Time.deltaTime;
-        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        timer += 1 * Time.deltaTime;
+        textoTimer.text = "" + timer.ToString("f0");
+
+        if(timer <= 120){
+            float peso1 = 50;
+            float peso2 = 30;
+            float peso3 = 19.8f;
+            float peso4 = 0.2f;
+            float pesoTotal = peso1 + peso2 + peso3 + peso4;
+            // float pesoAcumulado = 0;
+            float ranEnemy = Random.Range(0, pesoTotal);  
+            timeUntilSpawn -= Time.deltaTime;
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
 
 
-        if(timeUntilSpawn <= 0){
-            if(0f <= ranEnemy && ranEnemy <= peso1){
-                Instantiate(enemyPrefab, spawnPoints[randSpawnPoint].position, Quaternion.identity);
-                SetTimeUntilSpawn();
-            }
+            if(timeUntilSpawn <= 0){
+                if(0f <= ranEnemy && ranEnemy <= peso1){
+                    Instantiate(enemyPrefab, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
 
-            else if(ranEnemy >= peso1 && ranEnemy <= peso1+peso2){
-                Instantiate(enemyPrefab2, spawnPoints[randSpawnPoint].position, Quaternion.identity);
-                SetTimeUntilSpawn();
-            }
+                else if(ranEnemy >= peso1 && ranEnemy <= peso1+peso2){
+                    Instantiate(enemyPrefab2, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
 
-            else if(ranEnemy >= peso1+peso2 && ranEnemy <= peso1+peso2+peso3){
-                Instantiate(enemyPrefab3, spawnPoints[randSpawnPoint].position, Quaternion.identity);
-                SetTimeUntilSpawn();
-            }
+                else if(ranEnemy >= peso1+peso2 && ranEnemy <= peso1+peso2+peso3){
+                    Instantiate(enemyPrefab3, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
 
-            else if(ranEnemy >= peso1+peso2+peso3 && ranEnemy <= peso1+peso2+peso3+peso4){
-                Instantiate(enemyPrefab4, spawnPoints[randSpawnPoint].position, Quaternion.identity);
-                SetTimeUntilSpawn();
-            }
+                else if(ranEnemy >= peso1+peso2+peso3 && ranEnemy <= peso1+peso2+peso3+peso4){
+                    Instantiate(enemyPrefab4, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
             
+            }
         }
+
+        else if(timer>120 && timer<=300){
+            float peso1 = 25;
+            float peso2 = 45;
+            float peso3 = 27;
+            float peso4 = 3;
+            minimumSpawnTime=1;
+            maximumSpawnTime=3;
+            float pesoTotal = peso1 + peso2 + peso3 + peso4;
+            // float pesoAcumulado = 0;
+            float ranEnemy = Random.Range(0, pesoTotal);  
+            timeUntilSpawn -= Time.deltaTime;
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+
+            if(timeUntilSpawn <= 0){
+                if(0f <= ranEnemy && ranEnemy <= peso1){
+                    Instantiate(enemyPrefab, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+
+                else if(ranEnemy >= peso1 && ranEnemy <= peso1+peso2){
+                    Instantiate(enemyPrefab2, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+
+                else if(ranEnemy >= peso1+peso2 && ranEnemy <= peso1+peso2+peso3){
+                    Instantiate(enemyPrefab3, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+
+                else if(ranEnemy >= peso1+peso2+peso3 && ranEnemy <= peso1+peso2+peso3+peso4){
+                    Instantiate(enemyPrefab4, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+            
+            }
+        }
+
+        else if(timer>300){
+            float peso1 = 10;
+            float peso2 = 40;
+            float peso3 = 40;
+            float peso4 = 10;
+            minimumSpawnTime=0.5f;
+            maximumSpawnTime=2.5f;
+            float pesoTotal = peso1 + peso2 + peso3 + peso4;
+            // float pesoAcumulado = 0;
+            float ranEnemy = Random.Range(0, pesoTotal);  
+            timeUntilSpawn -= Time.deltaTime;
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+
+            if(timeUntilSpawn <= 0){
+                if(0f <= ranEnemy && ranEnemy <= peso1){
+                    Instantiate(enemyPrefab, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+
+                else if(ranEnemy >= peso1 && ranEnemy <= peso1+peso2){
+                    Instantiate(enemyPrefab2, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+
+                else if(ranEnemy >= peso1+peso2 && ranEnemy <= peso1+peso2+peso3){
+                    Instantiate(enemyPrefab3, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+
+                else if(ranEnemy >= peso1+peso2+peso3 && ranEnemy <= peso1+peso2+peso3+peso4){
+                    Instantiate(enemyPrefab4, spawnPoints[randSpawnPoint].position, Quaternion.identity);
+                    SetTimeUntilSpawn();
+                }
+            
+            }
+        }
+        
     }
 
     private void SetTimeUntilSpawn(){
