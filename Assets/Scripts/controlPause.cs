2f055214c89521d639b540ebee41db94795807db
suchT01 <zorrillaja30@gmail.com>
2024-06-18 16:19:22 -0400

    Documentacion

--- Assets/Scripts/controlPause.cs
+++ Assets/Scripts/controlPause.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class controlPause : MonoBehaviour
 {
+    // Se inicializa el canvas del menu
     public GameObject pausa;
+    // Se hace referencia al objeto del jugador, se pueden implementar mas cosas como objetos de enemigos etc
     public GameObject player;
     private RockyMovement rockyMovement;
     // Start is called before the first frame update
     void Start()
     {
+        // Se obtiene el script del player al iniciar el script
         rockyMovement = player.GetComponent<RockyMovement>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        // Funcion que detecta la tecla para el menu de Pausa
         if (Input.GetKeyDown(KeyCode.P))
         {
             Pausar();
         }
     }
 
+    // Funcion para pausar el juego
     private void Pausar()
     {
         bool valor = true;
@@ -29,6 +34,7 @@ public class controlPause : MonoBehaviour
         {
             pausa.SetActive(!valor);
             Time.timeScale = 1f;
+            // Condicion que reactiva el script del jugador
             rockyMovement.enabled = true;  // Reactivar el script de movimiento del jugador
         }
         else
